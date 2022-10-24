8ebc9902e1c5779a02eb2904a42e918c149f4ba1
Erick Otenyo <otenyo.erick@gmail.com>
2022-10-24 20:37:45 +0300

    add era5 data

--- ingest_scripts/era5_reanalysis_ingest.sh
+++ ingest_scripts/era5_reanalysis_ingest.sh
@@ -2,8 +2,8 @@
 set -xeu
 
 # ERA5
-/ingest.sh -n era5monthly_temperature_2_m -p /gskydata/mdi/era5monthly-temperature-2-m -t tif -x "-conf /rulesets/namespace_yyy-mm-ddTH.tif.json"
-/ingest.sh -n era5monthly_temperature_2_m_anomaly -p /gskydata/mdi/era5monthly-temperature-2-m-anomaly -t tif -x "-conf /rulesets/namespace_yyy-mm-ddTH.tif.json"
+/ingest.sh -n era5monthly_temperature_2_m -p /gskydata/era5/era5monthly-temperature-2-m -t tif -x "-conf /rulesets/namespace_yyy-mm-ddTH.tif.json"
+/ingest.sh -n era5monthly_temperature_2_m_anomaly -p /gskydata/era5/era5monthly-temperature-2-m-anomaly -t tif -x "-conf /rulesets/namespace_yyy-mm-ddTH.tif.json"
 
-/ingest.sh -n era5monthly_precipitation_1_day -p /gskydata/mdi/era5monthly-precipitation-1-day -t tif -x "-conf /rulesets/namespace_yyy-mm-ddTH.tif.json"
-/ingest.sh -n era5monthly_precipitation_1_day_anomaly -p /gskydata/mdi/era5monthly-precipitation-1-day-anomaly -t tif -x "-conf /rulesets/namespace_yyy-mm-ddTH.tif.json"
+/ingest.sh -n era5monthly_precipitation_1_day -p /gskydata/era5/era5monthly-precipitation-1-day -t tif -x "-conf /rulesets/namespace_yyy-mm-ddTH.tif.json"
+/ingest.sh -n era5monthly_precipitation_1_day_anomaly -p /gskydata/era5/era5monthly-precipitation-1-day-anomaly -t tif -x "-conf /rulesets/namespace_yyy-mm-ddTH.tif.json"
